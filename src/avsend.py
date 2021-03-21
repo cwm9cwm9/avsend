a672446044bbd9bb6053527936af2bb5ad62d521
cwm9cwm9 <github.reg@chiem.ma>
2021-03-20 19:22:14 -1000

    Update avsend.py
    Make Python 3 compatible

--- src/avsend.py
+++ src/avsend.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Python 3.6+
 
 import socket, sys, getopt, os
 
@@ -7,59 +8,60 @@ TCP_PORT = 50000
 BUFFER_SIZE = 1024
 
 def usage():
-        print "Usage:", os.path.basename(__file__),"[-h] [-v] [-a ADDRESS] [-u UNIT] -c COMMAND [ARGUMENTS]" 
-        print "Send YNCA commands via TCP to Yamaha AV Receiver models RX-V673, RX-A720, RX-V773, RX-A820, RX-A1020 etc."
-        print "" 
-        print "Flags:" 
-	print "-h, --help        print this help and exit" 
-        print "-v, --verbose     verbose output"
-        print "-a, --address     specify Receiver network name or IP (default is", TCP_IP,")" 
-        print "-u, --unit        specify a subunit: main, sys, zone2, tun, server, netradio, usb, ipodusb, airplay etc. Default is main"
-        print "-c, --command     command to send, e. g. vol, inp, mute, enhancer, soundprg, etc."
-        print "ARGUMENTS defaults to ?"
-        print ""
-        print "Examples:"
-        print "avsend -c vol         (same as avsend -c vol ?)"
-        print "avsend -c vol Up"
-        print "avsend -c vol -45.0   (be CAREFUL with positive numbers here!)"
-        print "avsend -c vol Up 5 dB"
-        print "avsend -c mute On"
-        print "avsend -c mute Off"
-        print "avsend -c inp HDMI1"
-        print "avsend -c soundprg 7ch Stereo" 
-        print "avsend -u server -c repeat All"
-        print "avsend -u server -c shuffle On"
-	print "avsend -u zone2 -c pwr On"
-	print "avsend -u sys -c party On"
-        print ""
-        print "For a complete list of available commands see http://thinkflood.com/media/manuals/yamaha/Yamaha-YNCA-Receivers.pdf"
-        print ""
-        print "Please modify the variable TCP_IP in this script to point at your receiver's ip (default is '" + TCP_IP +"')"
-        print "Default port is 50000 and should work out of the box."
+        print ("say what")
+        print ("Usage:", os.path.basename(__file__),"[-h] [-v] [-a ADDRESS] [-u UNIT] -c COMMAND [ARGUMENTS]")
+        print ("Send YNCA commands via TCP to Yamaha AV Receiver models RX-V673, RX-A720, RX-V773, RX-A820, RX-A1020 etc.")
+        print ("" )
+        print ("Flags:" )
+        print ("-h, --help        print this help and exit" )
+        print ("-v, --verbose     verbose output")
+        print ("-a, --address     specify Receiver network name or IP (default is", TCP_IP,")" )
+        print ("-u, --unit        specify a subunit: main, sys, zone2, tun, server, netradio, usb, ipodusb, airplay etc. Default is main")
+        print ("-c, --command     command to send, e. g. vol, inp, mute, enhancer, soundprg, etc.")
+        print ("ARGUMENTS defaults to ?")
+        print ("")
+        print ("Examples:")
+        print ("avsend -c vol         (same as avsend -c vol ?)")
+        print ("avsend -c vol Up")
+        print ("avsend -c vol -45.0   (be CAREFUL with positive numbers here!)")
+        print ("avsend -c vol Up 5 dB")
+        print ("avsend -c mute On")
+        print ("avsend -c mute Off")
+        print ("avsend -c inp HDMI1")
+        print ("avsend -c soundprg 7ch Stereo" )
+        print ("avsend -u server -c repeat All")
+        print ("avsend -u server -c shuffle On")
+        print ("avsend -u zone2 -c pwr On")
+        print ("avsend -u sys -c party On")
+        print ("")
+        print ("For a complete list of available commands see http://thinkflood.com/media/manuals/yamaha/Yamaha-YNCA-Receivers.pdf")
+        print ("")
+        print ("Please modify the variable TCP_IP in this script to point at your receiver's ip (default is '" + TCP_IP +"')")
+        print ("Default port is 50000 and should work out of the box.")
         sys.exit(2)
 
 def sendMessage(unit, command, arguments, verbose):
         msg = unit + ":" + command + "=" + arguments
-	if verbose: 
-	        print "send command " +  msg + " to " + TCP_IP + " port", TCP_PORT
+        if (verbose): 
+                print ("send command " +  msg + " to " + TCP_IP + " port", TCP_PORT)
         msg +=  "\r\n"
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(2.0)
+        s.settimeout(5.0)
         try:
                 s.connect((TCP_IP, TCP_PORT))
-                s.send(msg)
+                s.send(msg.encode())
                 data = s.recv(BUFFER_SIZE)
                 s.close
-                print data.rstrip()
+                print (data.rstrip())
         except socket.timeout as msg:
-                print "No connection to " + TCP_IP
+                print ("No connection to " + TCP_IP)
                 sys.exit(1)
 
 def main(argv):
         unit = "@MAIN"
         arguments = "?"
         command = ""
-	verbose = False;
+        verbose = False;
         for i,arg in enumerate(argv):
                 try:
                         f = float(arg)
@@ -78,8 +80,8 @@ def main(argv):
                 
         global TCP_IP
         for opt, arg in opts:
-		if opt in ("-v", "--verbose"):
-			verbose = True;
+                if opt in ("-v", "--verbose"):
+                        verbose = True;
                 if opt in ("-h", "--help"):
                         usage()
                 if opt in ("-u", "--unit"):
@@ -88,13 +90,12 @@ def main(argv):
                         command = arg.upper()
                 if opt in("-a", "--address"):
                         TCP_IP = arg
-                        print "new TCP IP: ", TCP_IP
-        if len(command) == 0:
-                print "Command is missing."
+                        print ("new TCP IP: ", TCP_IP)
+        if (len(command) == 0):
+                print ("Command is missing.")
                 usage()
 
         sendMessage(unit, command, arguments, verbose)
 
 if __name__ == "__main__":
         main(sys.argv[1:])
-
